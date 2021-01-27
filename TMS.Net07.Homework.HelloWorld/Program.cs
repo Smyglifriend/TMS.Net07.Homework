3389c8130e7f7a002c785f39c23b9220b3effff9
smyglifreind <heits_rs001@mail.ru>
2021-01-27 18:58:56 +0300

    task1

---
+++ TMS.Net07.Homework.HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            while (true)
+            {            
+                Console.WriteLine("Press number 0 to exit\n");
+                    Console.WriteLine("Enter the number: ");
+                    if (Int32.TryParse(Console.ReadLine(), out int number))
+                    {
+                        Console.WriteLine(number);
+                        Console.WriteLine("Excellent!!!");
+                    }
+                    else
+                        Console.WriteLine("String coulde not be parsed");
+                    if (number == 0)
+                        break;
+            }            
+        }
+    }
+}
